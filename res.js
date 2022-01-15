567e6ffd3d90b88731322a6c876dec9db76eba7c
zeral <zeri.aldi@gmail.com>
2022-01-15 20:39:14 +0700

    menambahkan controller.js

--- res.js
+++ res.js
@@ -1,10 +1,10 @@
 'use strict';
 
-export.ok = function(values,res){
-    let data ={
-        'status':200,
-        'values' ; values
-    };
-     res.json(data);
-     res.end();
-}
+exports.ok = function (values, res) {
+  let data = {
+    status: 200,
+    values: values,
+  };
+  res.json(data);
+  res.end();
+};
